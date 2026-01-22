577a5af407ab21470a051a18a433b7ddd1baf8c4
satakshi003 <soumya3955@gmail.com>
2026-01-22 22:30:49 +0530

    creating Button.jsx, Input.jsx, Header.jsx

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,2 +1,13 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "../Container/Container";
+import Logo from "./logo";
+import logoutBtn from "./Header/LogoutBtn";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  logoutBtn
+}
